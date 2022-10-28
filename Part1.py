4b7115427d128cd140a746b8b98c6bd8b9bfe180
saulms <105379798+saulms777@users.noreply.github.com>
2022-10-28 19:56:03 -0400

    Delete Part1.py

--- Part1.py
+++
@@ -1,9 +0,0 @@
-with open("Day1Input.txt") as file:
-    numbers = [int(line.strip()) for line in file.readline()]
-
-increase = 0
-for el in range(len(numbers) - 1):
-    if numbers[el] < numbers[el + 1]:
-        increase += 1
-
-print(increase)
